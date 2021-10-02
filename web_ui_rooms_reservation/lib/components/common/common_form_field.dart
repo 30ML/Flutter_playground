24e057e3d9a6dac94fcb97017c46786f50147563
mjdn0011 <exploit017@gmail.com>
2021-10-02 16:28:56 +0900

    common form field 구현

--- web_ui_rooms_reservation/lib/components/common/common_form_field.dart
+++ web_ui_rooms_reservation/lib/components/common/common_form_field.dart
@@ -1,8 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:web_ui_rooms_reservation/styles.dart';
 
 class CommonFormField extends StatelessWidget {
+  final prefixText;
+  final hintText;
+
+  const CommonFormField({required this.prefixText, required this.hintText});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        TextFormField(
+          // TextFormField 내부 세로 정렬
+          textAlignVertical: TextAlignVertical.bottom,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.only(top: 30, left: 20, bottom: 10),
+            hintText: hintText,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                color: Colors.black,
+                width: 2,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 8,
+          left: 20,
+          child: Text(
+            prefixText,
+            style: overLine(),
+          ),
+        ),
+      ],
+    );
   }
 }
