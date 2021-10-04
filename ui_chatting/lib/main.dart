fe28d922db0b02b5ca6377c1e7aea657b141b301
mjdn0011 <exploit017@gmail.com>
2021-10-04 16:30:13 +0900

    screens 세팅

--- ui_chatting/lib/main.dart
+++ ui_chatting/lib/main.dart
@@ -1,67 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:ui_chatting/screens/main_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBarTheme: AppBarTheme(
+          elevation: 0.0, // App Bar 하단 그림자 효과 제거
+          backgroundColor: Colors.white,
+          textTheme: TextTheme(
+            headline6: TextStyle(color: Colors.black, fontSize: 24),
+          ),
+          iconTheme: IconThemeData(color: Colors.black),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: MainScreen(),
     );
   }
 }
