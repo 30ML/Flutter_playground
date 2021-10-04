fec819f45ddd32ea8c6d9c80f80cd2ee49c9245c
mjdn0011 <exploit017@gmail.com>
2021-10-04 22:53:46 +0900

    chat screen 구현

--- ui_chatting/lib/screens/chat_screen.dart
+++ ui_chatting/lib/screens/chat_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:ui_chatting/models/chat.dart';
+import 'package:ui_chatting/components/chat_card.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('ChatScreen'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('채팅'),
+      ),
+      body: ListView(
+        children: List.generate(
+          chats.length,
+          (index) => ChatCard(chat: chats[index]),
+        ),
+      ),
     );
   }
 }
