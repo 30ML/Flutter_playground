16ab09539630f7e4a074730cd4ad8be549fce850
mjdn0011 <exploit017@gmail.com>
2021-10-04 17:01:04 +0900

    friends 화면 구현

--- ui_chatting/lib/screens/friend_screen.dart
+++ ui_chatting/lib/screens/friend_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ui_chatting/components/profile_card.dart';
+import 'package:ui_chatting/models/user.dart';
 
 class FriendScreen extends StatelessWidget {
   @override
@@ -6,6 +8,31 @@ class FriendScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(title: Text('친구')),
+      body: Column(
+        children: [
+          const SizedBox(height: 10),
+          ProfileCard(user: me),
+          Divider(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Row(
+              children: [
+                Text('친구'),
+                const SizedBox(width: 6),
+                Text('${friends.length}')
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: List.generate(
+                friends.length,
+                (index) => ProfileCard(user: friends[index]),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
