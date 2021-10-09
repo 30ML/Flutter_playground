c47f68d4baeaf33379b7ea9b29e9b8ae5bc734ce
mjdn0011 <exploit017@gmail.com>
2021-10-09 16:35:41 +0900

    나/상대 말풍선 ui 구현

---
+++ ui_chatting/lib/components/other_chat.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:ui_chatting/models/user.dart';
+
+class OtherChat extends StatelessWidget {
+  const OtherChat({
+    Key? key,
+    required this.name,
+    required this.text,
+    required this.time,
+  }) : super(key: key);
+
+  final String name;
+  final String text;
+  final String time;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(friends[0].backgroundImage),
+          ),
+          SizedBox(width: 10),
+          // TODO: ?
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(name),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(13),
+                    color: Colors.white,
+                  ),
+                  child: Text(text),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 5),
+          Text(time, style: TextStyle(fontSize: 12)),
+        ],
+      ),
+    );
+  }
+}
