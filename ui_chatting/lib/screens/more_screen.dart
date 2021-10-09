db50eb3842452016dde0f95982d47209045b7ab4
mjdn0011 <exploit017@gmail.com>
2021-10-09 17:21:45 +0900

    더보기 화면 구현 완료

--- ui_chatting/lib/screens/more_screen.dart
+++ ui_chatting/lib/screens/more_screen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:ui_chatting/models/tab.dart';
 
 class MoreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('MoreScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('더보기', style: TextStyle(color: Colors.black)),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 30),
+        child: GridView.count(
+          crossAxisCount: 4, // 한 줄에 아이템 4개
+          children: List.generate(
+            tabs.length,
+            (index) => Column(
+              children: [
+                Icon(tabs[index].icon),
+                SizedBox(height: 5),
+                Text(tabs[index].text),
+              ],
+            ),
+          ),
+        ),
+      ),
+      backgroundColor: Colors.white,
     );
   }
 }
