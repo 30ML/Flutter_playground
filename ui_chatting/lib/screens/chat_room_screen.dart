9043ae9c00d0b8a5992f04aab6b9e1b8e79004b2
mjdn0011 <exploit017@gmail.com>
2021-10-09 17:08:56 +0900

    입력한 나의 채팅 올라가도록 수정

--- ui_chatting/lib/screens/chat_room_screen.dart
+++ ui_chatting/lib/screens/chat_room_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:ui_chatting/components/my_chat.dart';
 import 'package:ui_chatting/components/other_chat.dart';
 import 'package:ui_chatting/components/time_line.dart';
+import 'package:ui_chatting/components/chat_icon_button.dart';
 
 class ChatRoomScreen extends StatefulWidget {
   @override
@@ -55,9 +57,52 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
                 ),
               ),
             ),
+            Container(
+              height: 60,
+              color: Colors.white,
+              child: Row(
+                children: [
+                  ChatIconButton(icon: Icon(FontAwesomeIcons.plusSquare)),
+                  Expanded(
+                    child: Container(
+                      child: TextField(
+                        controller:
+                            _textController, // 다른 곳에서 TextField 에 접근할 수 있음
+                        maxLines: 1,
+                        style: TextStyle(fontSize: 20),
+                        decoration: InputDecoration(
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                        ),
+                        onSubmitted:
+                            _handleSubmitted, // 텍스트 입력 후 완료 버튼을 눌렀을 때 일어나는 이벤트
+                      ),
+                    ),
+                  ),
+                  ChatIconButton(icon: Icon(FontAwesomeIcons.smile)),
+                  ChatIconButton(icon: Icon(FontAwesomeIcons.cog)),
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
+  }
+
+  void _handleSubmitted(text) {
+    _textController.clear(); // 전송(완료) 버튼 클릭 시 기존 입력됐던 텍스트 제거
+
+    setState(() {
+      chats.add(
+        MyChat(
+          text: text,
+          time: DateFormat('a K:m')
+              .format(new DateTime.now())
+              .replaceAll('AM', '오전')
+              .replaceAll('PM', '오후'),
+        ),
+      );
+    });
   }
 }
