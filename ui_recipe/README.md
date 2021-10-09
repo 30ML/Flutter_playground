8e1e44e5247eeaa629ba49240ad6248eada8a7be
mjdn0011 <exploit017@gmail.com>
2021-10-09 16:41:43 +0900

    ui_recipe 제거

--- ui_recipe/README.md
+++
@@ -1,2 +0,0 @@
-# flutter_ex_recipe
-플러터 UI 입문 - Recipe 앱 만들기 예제
--- ui_recipe/android/app/src/main/kotlin/com/example/flutter_ex_recipe/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_ex_recipe
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
