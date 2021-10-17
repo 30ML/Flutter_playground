c4511ff947ebcf8f955ba7fe6f641edc4e88b388
Min Jae Kim <exploit017@gmail.com>
2021-10-17 22:10:41 +0900

    main screen 위젯 생성

---
+++ used_market/lib/screens/main_screens.dart
@@ -0,0 +1,21 @@
+// IndexedStack, BottomNavigation 위젯을 가지는 파일
+
+import 'package:flutter/material.dart';
+
+class MainScreens extends StatefulWidget {
+  @override
+  _MainScreens createState() => _MainScreens();
+}
+
+class _MainScreens extends State<MainScreens> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: const Text(
+          'MainScreens',
+        ),
+      ),
+    );
+  }
+}
