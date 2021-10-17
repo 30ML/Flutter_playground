6d049121c20ceff04d4592c86368ede9d84a22d6
Min Jae Kim <exploit017@gmail.com>
2021-10-17 22:30:50 +0900

    앱 테마 설정

--- used_market/lib/theme.dart
+++ used_market/lib/theme.dart
@@ -1,1 +1,37 @@
 // 앱 테마 관리
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: textTheme(),
+    appBarTheme: appTheme(),
+  );
+}
+
+TextTheme textTheme() {
+  return TextTheme(
+    headline1: GoogleFonts.openSans(fontSize: 18.0, color: Colors.black),
+    headline2: GoogleFonts.openSans(
+        fontSize: 16.0, color: Colors.black, fontWeight: FontWeight.bold),
+    bodyText1: GoogleFonts.openSans(fontSize: 16.0, color: Colors.black),
+    bodyText2: GoogleFonts.openSans(fontSize: 14.0, color: Colors.grey),
+    subtitle1: GoogleFonts.openSans(fontSize: 15.0, color: Colors.black),
+  );
+}
+
+AppBarTheme appTheme() {
+  return AppBarTheme(
+    centerTitle: false,
+    color: Colors.white,
+    elevation: 0.0,
+    textTheme: TextTheme(
+      headline1: GoogleFonts.nanumGothic(
+        fontSize: 16.0,
+        fontWeight: FontWeight.bold,
+        color: Colors.black,
+      ),
+    ),
+  );
+}
