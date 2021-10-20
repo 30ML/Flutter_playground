58ab66d8f0cf31bfad82c9ecd451e63ed71f4953
Min Jae Kim <exploit017@gmail.com>
2021-10-20 18:09:03 +0900

    스크롤 시 로딩 구현 완료

--- http_test/lib/main.dart
+++ http_test/lib/main.dart
@@ -1,46 +1,122 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HttpApp(),
-    );
+    return MaterialApp(home: HttpApp());
   }
 }
 
 class HttpApp extends StatefulWidget {
   @override
-  _HttpApp createState() => _HttpApp();
+  _HttpAppState createState() => _HttpAppState();
 }
 
-class _HttpApp extends State<HttpApp> {
-  String _result = '';
+class _HttpAppState extends State<HttpApp> {
+  String result = '';
+  List? data;
+  TextEditingController? _textController;
+  ScrollController? _scrollController;
+  int page = 1;
+
+  Future<String> getJSONData() async {
+    var url =
+        'https://dapi.kakao.com/v3/search/book?target=title&page=$page&query=${_textController!.value.text}';
+    var response = await http.get(
+      Uri.parse(url),
+      headers: {'Authorization': 'KakaoAK 1a30fb14f2ca620aedd5875aa324a947'},
+    );
+
+    setState(() {
+      var dataConvertedFromJSON = json.decode(response.body);
+      List result = dataConvertedFromJSON['documents'];
+      data!.addAll(result);
+    });
+
+    return response.body;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    data = List.empty(growable: true);
+    _textController = TextEditingController();
+    _scrollController = ScrollController();
+    _scrollController!.addListener(() {
+      if (_scrollController!.offset >=
+              _scrollController!.position.maxScrollExtent &&
+          !_scrollController!.position.outOfRange) {
+        print('bottom');
+        page += 1;
+        getJSONData();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Http Example'),
-      ),
-      body: Container(
-        child: Center(
-          child: Text('$_result'),
+        title: TextField(
+          controller: _textController,
+          style: const TextStyle(color: Colors.white),
+          keyboardType: TextInputType.text,
+          decoration: const InputDecoration(hintText: '검색어를 입력하세요'),
         ),
       ),
+      body: Center(
+        child: data!.isEmpty
+            ? const Text(
+                '데이터가 없습니다.',
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.center,
+              )
+            : ListView.builder(
+                controller: _scrollController,
+                itemCount: data!.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Row(
+                      children: [
+                        Image.network(
+                          data![index]['thumbnail'],
+                          height: 100,
+                          width: 100,
+                          fit: BoxFit.contain,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width - 150,
+                              child: Text(
+                                data![index]['title'].toString(),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            Text(data![index]['authors'].toString()),
+                            Text(data![index]['sale_price'].toString()),
+                            Text(data![index]['status'].toString()),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          var url = 'http://www.google.com';
-          var response = await http.get(Uri.parse(url));
-
-          setState(() {
-            _result = response.body;
-          });
+        child: const Icon(Icons.file_download),
+        onPressed: () {
+          page = 1;
+          data!.clear();
+          getJSONData();
         },
-        child: Icon(Icons.file_download),
       ),
     );
   }
