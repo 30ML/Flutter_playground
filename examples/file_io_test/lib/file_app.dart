3eef7984817f8d916e6446b22e2f71b268e0b37e
Min Jae Kim <exploit017@gmail.com>
2021-10-25 03:13:28 +0900

    숫자 카운트 / 카운트 된 숫자 파일로 저장 및 불러오기 구현

--- examples/file_io_test/lib/file_app.dart
+++ examples/file_io_test/lib/file_app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 class FileApp extends StatefulWidget {
   @override
@@ -6,13 +8,55 @@ class FileApp extends StatefulWidget {
 }
 
 class _FileAppState extends State<FileApp> {
+  int _count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    readCountFile();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('File IO Example'),
       ),
-      body: SizedBox(),
+      body: Center(
+        child: Text(
+          '$_count',
+          style: const TextStyle(fontSize: 20),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            _count++;
+          });
+          writeCountFile(_count);
+        },
+      ),
     );
+  }
+
+  void writeCountFile(int count) async {
+    var dir = await getApplicationDocumentsDirectory();
+    File(dir.path + '/count.txt').writeAsStringSync(count.toString());
+  }
+
+  void readCountFile() async {
+    try {
+      var dir = await getApplicationDocumentsDirectory();
+      var file = File(dir.path + '/count.txt').readAsStringSync();
+      print(file);
+
+      setState(() {
+        _count = int.parse(file);
+      });
+    } catch (error) {
+      print(error.toString());
+    }
   }
 }
