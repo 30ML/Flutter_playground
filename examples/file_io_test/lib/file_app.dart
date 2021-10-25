1bbd56db853831c4118257cd8ba176e8f788b3f3
Min Jae Kim <exploit017@gmail.com>
2021-10-25 17:36:36 +0900

    파일에 저장된 내용을 공유 환경설정으로 불러오기/ListView로 뿌리기

--- examples/file_io_test/lib/file_app.dart
+++ examples/file_io_test/lib/file_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:io';
 
 class FileApp extends StatefulWidget {
@@ -9,12 +10,15 @@ class FileApp extends StatefulWidget {
 
 class _FileAppState extends State<FileApp> {
   int _count = 0;
+  List<String> itemList = List.empty(growable: true);
+  TextEditingController controller = TextEditingController();
 
   @override
   void initState() {
     super.initState();
 
     readCountFile();
+    initData();
   }
 
   @override
@@ -24,9 +28,28 @@ class _FileAppState extends State<FileApp> {
         title: const Text('File IO Example'),
       ),
       body: Center(
-        child: Text(
-          '$_count',
-          style: const TextStyle(fontSize: 20),
+        child: Column(
+          children: [
+            TextField(
+              keyboardType: TextInputType.text,
+              controller: controller,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: itemList.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Center(
+                      child: Text(
+                        itemList[index],
+                        style: const TextStyle(fontSize: 30),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -59,4 +82,45 @@ class _FileAppState extends State<FileApp> {
       print(error.toString());
     }
   }
+
+  void initData() async {
+    var result = await readListFile();
+    setState(() {
+      itemList.addAll(result);
+    });
+  }
+
+  Future<List<String>> readListFile() async {
+    List<String> itemList = List.empty(growable: true);
+    String key = 'first';
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    bool? isReaded = pref.getBool(key); // 파일을 처음 열었는지 확인
+
+    var dir = await getApplicationDocumentsDirectory();
+    bool isFileExist = await File(dir.path + '/fruit.txt').exists();
+
+    if (isReaded == null || isReaded == false || isFileExist == false) {
+      pref.setBool(key, true);
+      var file =
+          await DefaultAssetBundle.of(context).loadString('repo/fruit.txt');
+      File(dir.path + '/fruit.txt').writeAsStringSync(file);
+      var array = file.split('\n');
+
+      for (var item in array) {
+        print(item);
+        itemList.add(item);
+      }
+    } else {
+      var file = await File(dir.path + '/fruit.txt').readAsString();
+      var array = file.split('\n');
+
+      for (var item in array) {
+        print(item);
+        itemList.add(item);
+      }
+    }
+    return itemList;
+  }
+
+  void writeFruit(String fruit) async {}
 }
