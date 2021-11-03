77f61936ca1760084ee71f31ca0230c9d01c6491
Min Jae Kim <exploit017@gmail.com>
2021-11-03 10:55:22 +0900

    불필요한 폴더 삭제 및 폴더명 수정

---
+++ used_market_ui/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:used_market_ui/theme.dart';
+import 'package:used_market_ui/screens/main_screens.dart';
+
+void main() => runApp(UsedMarketUI());
+
+class UsedMarketUI extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'used_matket_ui',
+      debugShowCheckedModeBanner: false,
+      home: MainScreens(),
+      theme: theme(),
+    );
+  }
+}
