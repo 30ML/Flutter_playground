c54f632b6f74cf2a06ea1e6210ffc39a588e4317
Min Jae Kim <exploit017@gmail.com>
2021-11-03 14:38:30 +0900

    MainScreen > IndexedStack, BottomNavigationBar 추가

--- used_market_ui/lib/screens/main_screens.dart
+++ used_market_ui/lib/screens/main_screens.dart
@@ -1,6 +1,7 @@
 // IndexedStack, BottomNavigation 위젯을 가지는 파일
 
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class MainScreens extends StatefulWidget {
   @override
@@ -8,10 +9,54 @@ class MainScreens extends StatefulWidget {
 }
 
 class _MainScreensState extends State<MainScreens> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('MainScreens'),
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          Container(
+            // Index 0
+            color: Colors.orange[100],
+            child: Center(
+              child: Text(
+                'Indexed Stack 1',
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+            ),
+          ),
+          Container(
+            // Index 1
+            color: Colors.redAccent[100],
+            child: Center(
+              child: Text(
+                'Indexed Stack 2',
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        items: [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(CupertinoIcons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'Chat',
+            icon: Icon(CupertinoIcons.chat_bubble),
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
     );
   }
 }
