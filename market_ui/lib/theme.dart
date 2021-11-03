99d94998e55f3c91654fabf1f5d383c422eeb017
Min Jae Kim <exploit017@gmail.com>
2021-11-03 10:42:18 +0900

    app theme 설정

--- market_ui/lib/theme.dart
+++ market_ui/lib/theme.dart
@@ -1,1 +1,40 @@
 // 앱 테마 관리 파일
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextTheme textTheme() {
+  return TextTheme(
+    headline1: GoogleFonts.openSans(fontSize: 18.0, color: Colors.black),
+    headline2: GoogleFonts.openSans(
+      fontSize: 16.0,
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+    ),
+    bodyText1: GoogleFonts.openSans(fontSize: 16.0, color: Colors.black),
+    bodyText2: GoogleFonts.openSans(fontSize: 14.0, color: Colors.grey),
+    subtitle1: GoogleFonts.openSans(fontSize: 15.0, color: Colors.black),
+  );
+}
+
+AppBarTheme appbarTheme() {
+  return AppBarTheme(
+    centerTitle: false,
+    color: Colors.white,
+    elevation: 0.0,
+    // toolbarTextStyle: [textStyle]
+    toolbarTextStyle: GoogleFonts.nanumGothic(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+  );
+}
+
+ThemeData theme() {
+  return ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: textTheme(),
+    appBarTheme: appbarTheme(),
+  );
+}
