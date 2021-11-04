eccc36fabde21f4559b1504138cd6465f0ba0417
Min Jae Kim <exploit017@gmail.com>
2021-11-05 08:29:12 +0900

    HomeScreen 기본 코드 작성

--- used_market_ui/lib/screens/home/home_screen.dart
+++ used_market_ui/lib/screens/home/home_screen.dart
@@ -1,12 +1,24 @@
-// BottomNavigationBarItem의 홈 아이콘(Home)을 눌렀을 때 나타날 위젯
+// BottomNavigationBarItem의 '홈' 아이콘(Home)을 눌렀을 때 나타날 위젯
 
+import 'package:carrot_market_ui/theme.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomeScreen AppBar 영역 (index: 0)'),
+      ),
+      body: Container(
+        color: Colors.orange[100],
+        child: Center(
+          child: Text(
+            'HomeScreen Body 영역 (index: 0)',
+            style: textTheme().headline2,
+          ),
+        ),
+      ),
     );
   }
 }
