4e130d9d25c40070b74c531419a11b0d2613a007
Min Jae Kim <exploit017@gmail.com>
2021-11-04 18:59:59 +0900

    MainScreen > 남은 페이지 위젯 추가(동네 생활, 내 근처, 나의 정보)

--- used_market_ui/lib/screens/main_screens.dart
+++ used_market_ui/lib/screens/main_screens.dart
@@ -2,6 +2,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:used_market_ui/screens/chat/chat_screen.dart';
+import 'package:used_market_ui/screens/heighborhood_life/neighborhood_life_screen.dart';
+import 'package:used_market_ui/screens/home/home_screen.dart';
+import 'package:used_market_ui/screens/my_page/my_page_screen.dart';
+import 'package:used_market_ui/screens/near_me/near_me_screen.dart';
 
 class MainScreens extends StatefulWidget {
   @override
@@ -17,45 +22,44 @@ class _MainScreensState extends State<MainScreens> {
       body: IndexedStack(
         index: _selectedIndex,
         children: [
-          Container(
-            // Index 0
-            color: Colors.orange[100],
-            child: Center(
-              child: Text(
-                'Indexed Stack 1',
-                style: TextStyle(fontSize: 20, color: Colors.black),
-              ),
-            ),
-          ),
-          Container(
-            // Index 1
-            color: Colors.redAccent[100],
-            child: Center(
-              child: Text(
-                'Indexed Stack 2',
-                style: TextStyle(fontSize: 20, color: Colors.black),
-              ),
-            ),
-          ),
+          HomeScreen(),
+          NeighbothoodLifeScreen(),
+          NearMeScreen(),
+          ChatScreen(),
+          MyPageScreen(),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
         currentIndex: _selectedIndex,
-        items: [
-          BottomNavigationBarItem(
-            label: 'Home',
-            icon: Icon(CupertinoIcons.home),
-          ),
-          BottomNavigationBarItem(
-            label: 'Chat',
-            icon: Icon(CupertinoIcons.chat_bubble),
-          ),
-        ],
         onTap: (index) {
           setState(() {
             _selectedIndex = index;
           });
         },
+        items: const [
+          BottomNavigationBarItem(
+            label: '홈',
+            icon: Icon(CupertinoIcons.home),
+          ),
+          BottomNavigationBarItem(
+            label: '동네 생활',
+            icon: Icon(CupertinoIcons.square_on_square),
+          ),
+          BottomNavigationBarItem(
+            label: '내 근처',
+            icon: Icon(CupertinoIcons.placemark),
+          ),
+          BottomNavigationBarItem(
+            label: '채팅',
+            icon: Icon(CupertinoIcons.chat_bubble),
+          ),
+          BottomNavigationBarItem(
+            label: '나의 정보',
+            icon: Icon(CupertinoIcons.person),
+          ),
+        ],
       ),
     );
   }
