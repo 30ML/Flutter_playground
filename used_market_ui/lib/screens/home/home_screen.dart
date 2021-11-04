48010a681be95ec6b694c2cbb243139f7ec3d894
Min Jae Kim <exploit017@gmail.com>
2021-11-05 08:39:44 +0900

    HomeScreen > AppBar 작성

--- used_market_ui/lib/screens/home/home_screen.dart
+++ used_market_ui/lib/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 // BottomNavigationBarItem의 '홈' 아이콘(Home)을 눌렀을 때 나타날 위젯
 
 import 'package:carrot_market_ui/theme.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,7 +9,31 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomeScreen AppBar 영역 (index: 0)'),
+        title: Row(
+          children: const [
+            Text('영등포동'),
+            SizedBox(width: 4.0),
+            Icon(CupertinoIcons.chevron_down, size: 15),
+          ],
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(CupertinoIcons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(CupertinoIcons.list_dash),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(CupertinoIcons.bell),
+            onPressed: () {},
+          ),
+        ],
+        bottom: const PreferredSize(
+          preferredSize: Size.fromHeight(0.5),
+          child: Divider(thickness: 0.5, height: 0.5, color: Colors.grey),
+        ),
       ),
       body: Container(
         color: Colors.orange[100],
