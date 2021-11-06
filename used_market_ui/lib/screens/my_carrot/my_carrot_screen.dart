c9899c64ae9e649704ca3ac9ea246f2edda65daf
Min Jae Kim <exploit017@gmail.com>
2021-11-06 19:07:45 +0900

    HomeScreen 위젯 기본 코드 작성

--- used_market_ui/lib/screens/my_carrot/my_carrot_screen.dart
+++ used_market_ui/lib/screens/my_carrot/my_carrot_screen.dart
@@ -5,8 +5,35 @@ import 'package:flutter/material.dart';
 class MyCarrotScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('My Page screen'),
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: const Text('나의 당근'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {},
+          ),
+        ],
+        bottom: const PreferredSize(
+          preferredSize: Size.fromHeight(0.5),
+          child: Divider(thickness: 0.5, height: 0.5, color: Colors.grey),
+        ),
+      ),
+      body: ListView(
+        children: [
+          // MyCarrotHeader()
+          const SizedBox(height: 8.0),
+
+          // CardIconMenu(iconMenuList: iconMenu1),
+          const SizedBox(height: 8.0),
+
+          // CardIconMenu(iconMenuList: iconMenu2),
+          const SizedBox(height: 8.0),
+
+          // CardIconMenu(iconMenuList: iconMenu3),
+        ],
+      ),
     );
   }
 }
