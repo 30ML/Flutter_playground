3c5204cfdf82e60d5113523ff225d34859139aa9
Min Jae Kim <exploit017@gmail.com>
2021-11-08 16:17:15 +0900

    공 생성

---
+++ simple_pong/lib/ball.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Ball extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    const double diam = 50; // ball의 직경
+
+    return Container(
+      width: diam,
+      height: diam,
+      decoration: BoxDecoration(
+        color: Colors.amber[400],
+        shape: BoxShape.circle,
+      ),
+    );
+  }
+}
