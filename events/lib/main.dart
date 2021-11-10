ff184114d364daa990ff380d0305cae7230e9c8b
Min Jae Kim <exploit017@gmail.com>
2021-11-10 16:15:43 +0900

    빌드 성공(minSdkVersion 16 > 21 수정)

--- events/lib/main.dart
+++ events/lib/main.dart
@@ -3,13 +3,23 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 // Cloud Firestore 데이터베이스에 연결하고 디버그 콘솔에서 일부 데이터를 출력
-Future testData() async {}
+Future testData() async {
+  await Firebase.initializeApp(); // Firebase 인스턴스 생성
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  var data = await db.collection('event_details').get();
+  var details = data.docs.toList();
+  for (var detail in details) {
+    print('detail: ${detail.id}'); // 세부 정보 목록의 각 아이템에 대해 id 출력
+  }
+}
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    testData();
+
     return MaterialApp(
       title: 'Events',
       theme: ThemeData(
