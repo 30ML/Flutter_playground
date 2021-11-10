3445252a316ec257acfbae84d89b9cab9572d5dc
Min Jae Kim <exploit017@gmail.com>
2021-11-10 13:29:29 +0900

    ios 세팅
    pubspec 수정
    
    plist 보안 이슈로 gitignore 추가

--- events/lib/main.dart
+++ events/lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+// Cloud Firestore 데이터베이스에 연결하고 디버그 콘솔에서 일부 데이터를 출력
+Future testData() async {}
 
 void main() => runApp(MyApp());
 
