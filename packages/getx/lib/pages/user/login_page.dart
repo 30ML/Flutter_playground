08841b7c2677f2312f28dbb5d9a7efe1e2d2e093
Min Jae Kim <exploit017@gmail.com>
2021-11-18 08:43:57 +0900

    로그인 페이지 구현

--- packages/getx/lib/pages/user/login_page.dart
+++ packages/getx/lib/pages/user/login_page.dart
@@ -1,8 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:getx/components/custom_elevated_button.dart';
+import 'package:getx/components/custom_text_form_field.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Container(
+              height: 300,
+              alignment: Alignment.center,
+              child: Text(
+                '로그인 페이지',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            _loginForm(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _loginForm() {
+    return Form(
+      child: Column(
+        children: [
+          CustomTextFormField(hint: 'Username'),
+          CustomTextFormField(hint: 'Password'),
+          CustomElevatedButton(text: '로그인'),
+        ],
+      ),
+    );
   }
 }
