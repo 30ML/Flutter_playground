6b9da995552514bc0afb58e0e315e7d3d9580453
Min Jae Kim <exploit017@gmail.com>
2022-01-16 23:47:29 +0900

    day and nigth mood animation project

---
+++ animation/day_and_night_mood_animation/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:day_and_night_mood_animation/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Day & Night Moode Animation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
