b5b0df9be7788a1e57c7d645bfddd617bb4c9afd
Minjae Jay Kim <exploit017@gmail.com>
2022-02-23 20:38:47 +0900

    stuck

--- firebase/firebase_example/lib/main.dart
+++ firebase/firebase_example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,6 +8,8 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
 
   @override
   Widget build(BuildContext context) {
